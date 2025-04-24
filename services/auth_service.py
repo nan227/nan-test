d3ff61b0ce40d57d5732dbc2ced51b98c7cf9d5f
nan227 <brakyony@gmail.com>
2025-04-24 15:19:28 +0600

    Delete services directory

--- services/auth_service.py
+++
@@ -1,3 +0,0 @@
-def authenticate_user(username, password):
-    # Logic for authenticating users
-    pass
--- services/book_service.py
+++
@@ -1,3 +0,0 @@
-def calculate_late_fee(issue_date, return_date):
-    # Logic to calculate late fee
-    pass
